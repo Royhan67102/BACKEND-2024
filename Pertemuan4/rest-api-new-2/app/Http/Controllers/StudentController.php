93ac70b7da9b81d3b30104511af172d79534e197
Royhan67102 <royhanaudy14@gmail.com>
2024-10-29 11:43:14 +0700

    tugas update&delete students

---
+++ Pertemuan4/rest-api-new-2/app/Http/Controllers/StudentController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Student;
+
+class StudentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $students = Student::all();
+
+        $response = [
+            'data' => $students,
+            'message' => 'Berhasil menampilkan semua data students'
+        ];
+
+        return response()->json($response, 200);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $input = [
+            'name' => $request->name,
+            'nim' => $request->nim,
+            'email' => $request->email,
+            'jurusan' => $request->jurusan
+           ];
+
+           $students = Student::create($input);
+
+           $response = [
+            'message' => 'Successfully create new student',
+            'data' => $students
+           ];
+
+           return response()->json($response, 201);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $student = Student::find($id);
+
+        if (!$student) {
+            return response()->json(['message' => 'Siswa tidak ditemukan'], 404);
+        }
+
+        $student->update($request->only(['name', 'nim', 'email', 'jurusan']));
+
+        return response()->json(['message' => 'Data siswa berhasil diperbarui', 'data' => $student], 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $student = Student::find($id);
+
+        if (!$student) {
+            return response()->json(['message' => 'Siswa tidak ditemukan'], 404);
+        }
+
+        $student->delete();
+
+        return response()->json(['message' => 'Siswa berhasil dihapus'], 200);
+    }
+}
